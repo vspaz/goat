4583b2c4dab48b4614d8d1fd566e37b0a7d6668f
vspaz <vladimir.edelman@gmail.com>
2021-10-24 19:29:00 +0200

    add 'clientBuilder'

--- pkg/ghttp/builder.go
+++ pkg/ghttp/builder.go
@@ -7,8 +7,27 @@ type ClientBuilder interface {
 	Auth(user string, password string) ClientBuilder
 	Tls(certFilePath, keyFilePath, CaFilePath string) ClientBuilder
 	UserAgent(ua string) ClientBuilder
-	Retry(num int) ClientBuilder
+	RetryCount(num int) ClientBuilder
 	Delay(num float64) ClientBuilder
 	ConnTimeout(timeout time.Duration) ClientBuilder
 	ReadTimeout(timeout time.Duration) ClientBuilder
+}
+
+type clientBuilder struct {
+	host string
+
+	basicAuthUser     string
+	basicAuthPassword string
+
+	tlsCertFilePath string
+	tlsKeyFilePath  string
+	tlsCaFilePath   string
+
+	userAgent string
+
+	retryCount int
+	delay      float64
+
+	connTimeout time.Duration
+	readTimeout time.Duration
 }
