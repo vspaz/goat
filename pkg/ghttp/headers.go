bd489b14968614b9b5d98061d80989ff549692d3
vspaz <vladimir.edelman@gmail.com>
2021-10-24 21:15:55 +0200

    add helper methods to idenfity header type

--- pkg/ghttp/headers.go
+++ pkg/ghttp/headers.go
@@ -8,3 +8,13 @@ func setHeaders(r *http.Request, headers map[string]string) *http.Request {
 	}
 	return r
 }
+
+func isJson(headers map[string]string) bool {
+	contentType, _ := headers["Content-Type"]
+	return contentType == "application/json"
+}
+
+func isPlainTest(headers map[string]string) bool{
+	contentType, _ := headers["Content-Type"]
+	return contentType == "text/plain" || contentType == "text/html"
+}
