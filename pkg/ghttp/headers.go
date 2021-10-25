f33499b4c79839f764cd033b9fa0f90b8505525d
vspaz <vladimir.edelman@gmail.com>
2021-10-25 17:21:17 +0200

    fix warnigns

--- pkg/ghttp/headers.go
+++ pkg/ghttp/headers.go
@@ -10,11 +10,6 @@ func setHeaders(r *http.Request, headers map[string]string) *http.Request {
 }
 
 func isJson(headers map[string]string) bool {
-	contentType, _ := headers["Content-Type"]
-	return contentType == "application/json"
-}
-
-func isPlainTest(headers map[string]string) bool {
-	contentType, _ := headers["Content-Type"]
-	return contentType == "text/plain" || contentType == "text/html"
+	contentType, hasKey := headers["Content-Type"]
+	return hasKey && contentType == "application/json"
 }
