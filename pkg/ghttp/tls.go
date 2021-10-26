d7d94458fb204ab3f3bdc7a7cfb4e0c97376a74e
vspaz <vladimir.edelman@gmail.com>
2021-10-26 09:43:13 +0200

    simplify tls

--- pkg/ghttp/tls.go
+++ pkg/ghttp/tls.go
@@ -6,24 +6,33 @@ import (
 	"io/ioutil"
 )
 
-func createTlsConfig(certFilePath, keyFilePath, caFilePath string) *tls.Config {
-	if certFilePath == "" && keyFilePath == "" && caFilePath == "" {
-		return &tls.Config{}
+func loadKeyPair(certFilePath string, keyFilePath string) tls.Certificate {
+	if certFilePath == "" && keyFilePath == "" {
+		return tls.Certificate{}
 	}
 	certs, err := tls.LoadX509KeyPair(certFilePath, keyFilePath)
 	if err != nil {
 		panic(err)
 	}
+	return certs
+}
 
+func loadCa(caFilePath string) *x509.CertPool {
+	if caFilePath == "" {
+		return &x509.CertPool{}
+	}
 	ca, err := ioutil.ReadFile(caFilePath)
 	if err != nil {
 		panic(err)
 	}
 	caCertPool := x509.NewCertPool()
 	caCertPool.AppendCertsFromPEM(ca)
+	return caCertPool
+}
 
+func createTlsConfig(certFilePath, keyFilePath, caFilePath string) *tls.Config {
 	return &tls.Config{
-		Certificates: []tls.Certificate{certs},
-		RootCAs:      caCertPool,
+		Certificates: []tls.Certificate{loadKeyPair(certFilePath, keyFilePath)},
+		RootCAs:      loadCa(caFilePath),
 	}
 }
